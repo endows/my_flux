e09141e61eee15d7d4a43a2db1d43eaea227ee4a
endows <you@example.com>
2015-06-26 07:15:22 +0900

    list each and push it

--- build/bundle.js
+++ build/bundle.js
@@ -55,9 +55,20 @@
 	var demo = new Vue({
 	  el: '#demo',
 	  data: {
-	    message: 'Hello Vuee'
+	    count: 1
+	  },
+	  methods:{
+	    inc:function(){
+	      this.count ++
+	    }
 	  }
 	});
+
+	setInterval(function(){
+	  // demo.inc()
+	  demo.count = demo.count + 1
+	  console.log('plued')
+	},1000)
 
 
 /***/ },
